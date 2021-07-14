170ebb72778b76c9860e2884374a2c301190a72c
Lior Lustgarten <lilustga@microsoft.com>
2021-07-14 15:05:30 -0700

    Fixed type comparison and exported symbols

--- src/blackboard.cpp
+++ src/blackboard.cpp
@@ -22,7 +22,7 @@ void Blackboard::setPortInfo(std::string key, const PortInfo& info)
     }
     else{
         auto old_type = it->second.port_info.type();
-        if( old_type && old_type != info.type() )
+        if( old_type && *old_type != *info.type() )
         {
             throw LogicError( "Blackboard::set() failed: once declared, the type of a port shall not change. "
                              "Declared type [",     BT::demangle( old_type ),
